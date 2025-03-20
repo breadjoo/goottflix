b9cc83112a1000b60dddb0ac2db6068578eeefd1
breadjoo <yoohwanjoo@nate.com>
2025-03-20 14:17:03 +0900

    실시간채팅 파일업로드 주소 및 방식 변경

--- src/main/java/com/goottflix/chat/controller/FileUploadController.java
+++ src/main/java/com/goottflix/chat/controller/FileUploadController.java
@@ -1,6 +1,8 @@
 package com.goottflix.chat.controller;
 
 
+import com.goottflix.common.FileService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,27 +18,19 @@ import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api/upload")
+@RequiredArgsConstructor
 public class FileUploadController {
 
     private static final String UPLOAD_DIR = "uploads/";
+    private final FileService fileService;
 
     @PostMapping("/image")
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
         try {
-            // 파일명을 시스템의 현재 시간 + 원본 파일명으로 설정
-            String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
-            Path filePath = Paths.get(UPLOAD_DIR + fileName);
-
-            // 디렉토리 생성 및 파일 저장
-            Files.createDirectories(filePath.getParent());
-            Files.write(filePath, file.getBytes());
-
-            // 업로드된 파일 URL 반환
-            String fileUrl = "http://localhost:8080/" + UPLOAD_DIR + fileName;
+            String fileUrl = fileService.saveFile(file);
             return ResponseEntity.ok(fileUrl);
-
         } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("파일 업로드 실패");
+            return ResponseEntity.internalServerError().body("파일 업로드 실패: " + e.getMessage());
         }
     }
 }
